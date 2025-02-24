9a9f80bf8ba7e9cacfe7a88d998c93fe3672341f
arimu <arimurahiroaki40@gmail.com>
2025-02-24 20:49:03 +0900

    session情報がサーバーに送信されていない問題を修正中

--- middleware.ts
+++ middleware.ts
@@ -1,10 +1,12 @@
-import { createMiddlewareClient } from "@supabase/auth-helpers-nextjs";
-import { NextResponse } from "next/server";
-import type { NextRequest } from "next/server";
+// middleware.ts (プロジェクトのルート直下に配置)
+import { createMiddlewareClient } from "@supabase/auth-helpers-nextjs"
+import { NextResponse } from "next/server"
+import type { NextRequest } from "next/server"
 
 export async function middleware(req: NextRequest) {
-  const res = NextResponse.next();
-  const supabase = createMiddlewareClient({ req, res });
-  await supabase.auth.getSession();
-  return res;
+  const res = NextResponse.next()
+  const supabase = createMiddlewareClient({ req, res })
+  // セッション情報を初期化（Cookieから読み込む）
+  await supabase.auth.getSession()
+  return res
 }
